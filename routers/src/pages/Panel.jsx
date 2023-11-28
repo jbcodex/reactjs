3a8bfe056201910bb68024ea7c58113f6cf98747
Jarbas Bento <jbdev@tutanota.com>
2023-11-28 16:21:49 -0300

    Painel de cadastro criado

--- routers/src/pages/Panel.jsx
+++ routers/src/pages/Panel.jsx
@@ -1,7 +1,44 @@
-
+import { useState } from "react"
+import "./Panel.css"
 const Panel = () => {
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
+  const [description, setDescription] = useState("")
+  const [info, setInfo] = useState("")
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    const product ={
+      name,
+      price,
+      description,
+      info
+    }
+  }
+
+  
   return (
-    <div>Panel</div>
+    <div>
+      <form onSubmit={handleSubmit} autoComplete="off">
+        <label>
+          <span>Nome Produto</span>
+          <input type="text" name="name" value={name} onChange={(e)=>setName(e.target.value)} />
+        </label>
+        <label>
+          <span>Preço</span>
+          <input type="text" name="price" value={price} onChange={(e)=>setPrice(e.target.value)} />
+        </label>
+        <label>
+          <span>Descrição</span>
+          <input type="text" name="description" value={description} onChange={(e)=>setDescription(e.target.value)} />
+        </label>
+        <label>
+          <span>Info adicional</span>
+          <input type="text" name="info" value={info} onChange={(e)=>setInfo(e.target.value)} />
+        </label>
+        <input className="btn" type="submit" value="Cadastrar" />
+      </form>
+    </div>
   )
 }
 
