71a9d73fd37d95241f0ca22499604c4f9c25ce89
Jarbas Bento <jbdev@tutanota.com>
2023-12-04 10:56:30 -0300

    Estilos do Navbar e rodapé

---
+++ undercode/src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+//Pages
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+
+//Components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
