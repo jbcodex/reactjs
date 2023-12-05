2af47c5763cf5ab9a6171393850c2bfb1661c2fa
Jarbas Bento <jbdev@tutanota.com>
2023-12-05 19:34:03 -0300

    Página About criada

--- undercode/src/pages/About/About.jsx
+++ undercode/src/pages/About/About.jsx
@@ -2,7 +2,11 @@ import styles from "./About.module.css"
 const About = () => {
   return (
     <>
-        <h1>About</h1>
+        <div className={styles.about}>
+        <h2>Sobre nós</h2>
+        <p>Bem-vindo ao Under<b>CODE</b>, seu guia confiável no universo da segurança digital. Nossa missão é pioneira: oferecer compreensão completa das ameaças cibernéticas e soluções inovadoras. De análises detalhadas a estratégias avançadas, mantemos você à frente nas últimas tendências. Junte-se à nossa comunidade e descubra como podemos tornar o mundo digital mais seguro, uma linha de código de cada vez.</p>
+        
+        </div>
     </>
   )
 }
