3bc6f5f761b67f4efd82d6cc245f8a83d55fd289
Jarbas Bento <jbdev@tutanota.com>
2023-12-06 07:06:25 -0300

    Inicio form de login

--- undercode/src/pages/Login/Login.jsx
+++ undercode/src/pages/Login/Login.jsx
@@ -1,8 +1,20 @@
 import styles from "./Login.module.css"
 const Login = () => {
   return (
-    <div>
-        <h2>Login</h2>
+    <div className={styles.login}>
+      <h1>Faça login para acessar</h1>
+        <form>
+         <label>
+          <span>Usuário: </span>
+          <input type="text" name="user" />
+         </label>
+         <label>
+          <span>Senha: </span>
+          <input type="password" name="password" />
+         </label>
+         <input className="btn" type="submit" value="Entrar" />
+        </form>
+
     </div>
   )
 }
