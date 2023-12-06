54f5773fef0b1ac091c56779426926aa9faf7a8c
Jarbas Bento <jbdev@tutanota.com>
2023-12-06 09:13:13 -0300

    CSS do formulário de login

--- undercode/src/pages/Login/Login.jsx
+++ undercode/src/pages/Login/Login.jsx
@@ -1,22 +1,99 @@
-import styles from "./Login.module.css"
+import { useState, useEffect } from "react";
+import { useAuthentication } from "../../hooks/userAuthentication";
+import { FaLock, FaUser } from "react-icons/fa";
+import styles from "./Login.module.css";
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const {
+    createUser,
+    error: authError,
+    loading,
+    success,
+  } = useAuthentication();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    const user = {
+      email,
+      password,
+    };
+    const response = await createUser(user);
+  };
+
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
+    }
+  }, [authError]);
+
+
   return (
     <div className={styles.login}>
-      <h1>Faça login para acessar</h1>
-        <form>
-         <label>
+      <h1>Login</h1>
+      <p>Entre para criar postagens!</p>
+      <form onSubmit={handleSubmit}>
+        <label>
           <span>Usuário: </span>
-          <input type="text" name="user" />
-         </label>
-         <label>
+          <FaUser
+            style={{
+              position:"absolute",
+              top:"265px",
+              right:"35%",
+              color: "#aaa",
+              fontSize: "1em",
+              marginBottom: "-2px",
+              marginRight: "5px",
+              cursor:"pointer",
+              backgroundColor:"transparent"
+            }}
+            
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="E-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
           <span>Senha: </span>
-          <input type="password" name="password" />
-         </label>
-         <input className="btn" type="submit" value="Entrar" />
-        </form>
+          <FaLock
+            style={{
+              position:"absolute",
+              top:"345px",
+              right:"35%",
+              color: "#aaa",
+              fontSize: "1em",
+              marginBottom: "-2px",
+              marginRight: "5px",
+              cursor:"pointer",
+              backgroundColor:"transparent"
+            }}
+            
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        {!loading && <button className="btn">Entrar</button>}
+        {loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
 
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
