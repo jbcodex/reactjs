1b62c3c742b80a9e8d7a5afe0544c54ee4d71afb
Jarbas Bento <jbdev@tutanota.com>
2023-12-09 15:38:35 -0300

    Post individual criado

--- undercode/src/hooks/useFetchDocuments.jsx
+++ undercode/src/hooks/useFetchDocuments.jsx
@@ -48,7 +48,7 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
       }
     };
     loadData();
-  }, [docCollection, search, uid, cancelled]);
+  }, [docCollection, search, uid, cancelled, documents]);
 
   useEffect(() => {
     return () => setCancelled(true);
--- undercode/src/pages/Post/Post.jsx
+++ undercode/src/pages/Post/Post.jsx
@@ -1,12 +1,23 @@
 import styles from "./Post.module.css"
+import { useFetchDocument } from "../../hooks/useFetchDocument";
 
 //hooks
 import { useParams } from "react-router-dom";
 const Post = () => {
     const { id } = useParams();
+    const { document: post} = useFetchDocument("posts", id)
   return (
     <div>
-        <h1>Post{id}</h1>
+       
+        {post && (
+            <>
+               <div className={styles.post}>
+                 <h2>{post.title}</h2>
+                 <img src={post.image} alt="" />
+                 <p>{post.body}</p>
+               </div>
+            </>
+        )}
     </div>
   )
 }
