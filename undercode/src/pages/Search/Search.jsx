b2aa3a02c03a3dece5953204a0fe08b02f262b9f
Jarbas Bento <jbdev@tutanota.com>
2023-12-09 14:47:04 -0300

    Ajustes search not found

--- undercode/src/pages/Search/Search.jsx
+++ undercode/src/pages/Search/Search.jsx
@@ -4,6 +4,8 @@ import { useQuery } from "../../hooks/useQuery"
 import FetchPosts from "../../components/FetchPosts"
 import { Link } from "react-router-dom"
 import { FaPencilAlt} from "react-icons/fa";
+import { RiEmotionSadFill } from "react-icons/ri";
+import { IoMdArrowRoundBack } from "react-icons/io";
 
 
 const Search = () => {
@@ -17,9 +19,10 @@ const Search = () => {
       <h3>Search</h3>
           {posts && posts.length === 0  &&(
             <>
-              <p>Não foram encontrados Posts sobre <b>{search}</b></p>
+           
+              <p> <RiEmotionSadFill style={{color:"orange", marginBottom:"-3px", fontSize:"18px"}} /> Desculpe, não foram encontrados Posts sobre <b>{search}</b></p>
               <Link to="/posts/create" style={{color:"green"}}><FaPencilAlt style={{fontSize:"12px", marginRight:"5px"}} /><b>Criar post</b></Link>
-              <Link to="/" className="btn" style={{width:"100px"}}><b>Voltar</b></Link>
+              <Link to="/" className="btn" style={{width:"100px"}}><IoMdArrowRoundBack style={{marginBottom:"-3px", fontSize:"18px"}} /> <b>Voltar</b></Link>
             </>
           ) }
           {posts && posts.map((post) =>(
