2e28414fc352b96ab8f402f6a09f297b0ed602db
Jarbas Bento <jbdev@tutanota.com>
2023-12-15 07:56:16 -0300

    Formulário atualizado

--- cadalunos/src/components/NavBar.jsx
+++ cadalunos/src/components/NavBar.jsx
@@ -12,11 +12,13 @@ const NavBar = () => {
     <nav className="navbar">
       
         <span><HiMenu style={{fontSize:"26px", color:"#fff"}} /> <h3><b style={{color:"yellow"}}>Escola Técnica Felícia Rangel</b></h3><p>&nbsp;{user && " Olá "+user.displayName.split(" ")[0]}</p> </span>
-        <div className="search">
-          <form className="searchForm">
-             <input className="searchInput" type="text" name="student" placeholder="Busque por aluno ou turma" /> <button className="btnSearch"><IoSearchSharp/></button>
-         </form>
-        </div>
+        {user && (
+            <div className="search">
+            <form className="searchForm">
+               <input className="searchInput" type="text" name="student" placeholder="Busque por aluno ou turma"  required/> <button className="btnSearch"><IoSearchSharp/></button>
+           </form>
+          </div>
+        )}
        <ul>
           {user && <li><Link to="/">Início</Link></li>}
           {user &&  <li><Link to="/register">Cadastro de usuários</Link></li>}
